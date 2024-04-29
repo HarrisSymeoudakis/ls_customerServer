97e2365875ecec09edbf08d4c9dc3e2847af1a9b
Harris Symeoudakis <harris.symeoudakis@gmail.com>
2024-04-29 22:02:21 +0300

    Update customer_server.js

--- customer_server.js
+++ customer_server.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 10010;
+const port = process.env.PORT || 3000;
+
+let latestData = {}; // Variable to store the latest JSON data
 
 app.use(express.json());
 
 app.post('/webhook', (req, res) => {
   // Assuming your JSON data is received in the req.body
-  const jsonData = req.body;
-
-  // Log the JSON data
-  console.log('Received JSON data:', jsonData);
-
-  // Assuming you want to send back the same JSON data received
-  res.json(jsonData);
+  latestData = req.body;
+  console.log('Received JSON data:', latestData);
+  res.status(200).send('Webhook received successfully');
 
   // Send a response back to the sender system
   // You can customize the response as needed
   res.status(200).send('Webhook received successfully');
 });
+
+// Endpoint to serve the latest JSON data
+app.get('/latest', (req, res) => {
+  res.json(latestData);
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
