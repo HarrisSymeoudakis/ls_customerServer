b0a18df9398531e5049bc66c870c2c8d7954c220
Harris Symeoudakis <harris.symeoudakis@gmail.com>
2024-04-29 22:20:57 +0300

    Update customer_server.js

--- customer_server.js
+++ customer_server.js
@@ -5,18 +5,10 @@ const port = process.env.PORT || 3000;
 let latestData = {}; // Variable to store the latest JSON data
 
 app.use(function(req, res, next) {
-      // res.header("Access-Control-Allow-Origin", "*");
-      const allowedOrigins = ['http://localhost:3000', 'https://ls-customerserver.onrender.com', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com'];
-      const origin = req.headers.origin;
-      if (allowedOrigins.includes(origin)) {
-           res.setHeader('Access-Control-Allow-Origin', origin);
-      }
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-      res.header("Access-Control-Allow-credentials", true);
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-      console.log('cors init');
-      next();
-    });
+  res.header("Access-Control-Allow-Origin", "*"); // Update * to your specific origin if needed
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json());
 
