67dafffadd74ce6d91044ef38e8bed97e004b567
Charalampos Symeoudakis <CSymeoudakis@mspos.net>
2024-04-29 18:20:37 +0300

    Initial commit

---
+++ customer_server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 10010;
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+  // Assuming your JSON data is received in the req.body
+  const jsonData = req.body;
+
+  // Assuming you want to send back the same JSON data received
+  res.json(jsonData);
+
+  // Send a response back to the sender system
+  // You can customize the response as needed
+  res.status(200).send('Webhook received successfully');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
