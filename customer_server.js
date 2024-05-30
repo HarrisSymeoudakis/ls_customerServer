60129e15adf10a4811961e7c6f9ff958c9396eda
Harris Symeoudakis <harris.symeoudakis@gmail.com>
2024-05-30 18:40:55 +0300

    Update customer_server.js

--- customer_server.js
+++ customer_server.js
@@ -161,6 +161,82 @@ app.get('/swagger/Addresses', async (req, res) => {
   }
 });
 
+// --------------------- --------------------- SEND PAYLOAD BACK TO Livestore - External Customer form
+
+app.use(bodyParser.json());
+
+app.post('/api/PostBackExtForm', async (req, res) => {
+    const { sessionId } = req.body;
+    
+    const endpoint = `https://retail-services.cegid.cloud/et/pos/pulsarcustomer/externalcustomer/session?sessionId=${sessionId}`;
+    
+    const postData = {
+        "languageIsoCode": "en",
+        "customerId": "DE01000021",
+        "firstName": "Harris",
+        "lastName": "Symeoudakis",
+        "gender": "MALE",
+        "titleCode": "1",
+        "birthDay": 20,
+        "birthMonth": 3,
+        "birthYear": 1992,
+        "emails": [
+            {
+                "email": "harris.symeoudakis@gmail.com",
+                "emailType": "Main",
+                "emailConsent": "DEM",
+                "qualityCode": null
+            }
+        ],
+        "phones": [
+            {
+                "phone": "555252",
+                "phoneType": "Mobile",
+                "phoneConsent": "DEM",
+                "qualityCode": null
+            }
+        ],
+        "postalAddresses": [
+            {
+                "addressLine1": "Zoodohou Pigis 6",
+                "addressLine2": "",
+                "addressLine3": "",
+                "addressLine4": null,
+                "addressLine5": null,
+                "addressLine6": null,
+                "addressLine7": null,
+                "addressLine8": null,
+                "addressLine9": null,
+                "city": "Heraklion,Crete",
+                "postalCode": "71307",
+                "region": "PACA",
+                "country": {
+                    "countryIso2Code": "GR",
+                    "countryLabel": "Greece"
+                },
+                "addressConsent": true,
+                "qualityCode": null,
+                "languageIsoCode": "end"
+            }
+        ]
+    };
+
+    try {
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postData)
+        });
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
